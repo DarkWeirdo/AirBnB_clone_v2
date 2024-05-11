736b24f99e68d7471e375d9299f169d2ff9ed899
DarkWeirdo <sonic_unleashed@live.com>
2024-05-11 19:16:07 +0300

    task 2 deploy_web_static fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static
-folder of your AirBnB Clone repo, using the function do_pack.
-"""
-from fabric.api import local, put, run, env
+
+from fabric.api import local, run, put, env
 from datetime import datetime
 import os
 
+# Define the hosts where the deployment will occur
 env.hosts = ["54.157.191.51", "52.91.116.19"]
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    """
-    try:
-        date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        if not os.path.exists("versions"):
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date_time)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception:
+    """Generates a.tgz archive"""
+    local("mkdir -p versions")
+    file_name = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    result = local("tar -cvzf {} web_static".format(file_name))
+    if result.failed:
         return None
+    return file_name
 
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to the web servers.
+    Distributes an archive to web servers
     """
-    if not os.path.exists(archive_path):
+    if not os.path.isfile(archive_path):
         return False
     try:
+        # Extract the file name from the archive_path
         file_name = archive_path.split("/")[-1]
-        name = file_name.split(".")[0]
-        path = "/data/web_static/releases/{}/".format(name)
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, path))
+        # Remove the extension from the file_name
+        no_ext = file_name.split(".")[0]
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/{}".format(file_name))
+        # Uncompress the archive to the folder on the web server
+        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+        run(
+            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+                file_name, no_ext
+            )
+        )
+        # Delete the archive from the web server
         run("rm /tmp/{}".format(file_name))
-        run("mv {0}web_static/* {0}".format(path))
-        run("rm -rf {}web_static".format(path))
+        # Move the content to the final location
+        run(
+            "mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(no_ext, no_ext)
+        )
+        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
+        # Delete the symbolic link from the web server
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(path))
+        # Create a new the symbolic link on the web server
+        run(
+            "ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(no_ext)
+        )
         print("New version deployed!")
         return True
     except Exception:
@@ -51,9 +63,13 @@ def do_deploy(archive_path):
 
 def deploy():
     """
-    Creates and distributes an archive to the web servers.
+    Deploys the web_static to the web servers
     """
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
     return do_deploy(archive_path)
+
+
+if __name__ == "__main__":
+    deploy()
